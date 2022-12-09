1a5e7d146538d156da63aaced6b7b840e876ce08
dmsRosa <91638513+dmsRosa@users.noreply.github.com>
2022-12-09 17:50:00 +0000

    mongo

--- src/scc/data/MongoDBLayer.java
+++ src/scc/data/MongoDBLayer.java
@@ -4,6 +4,7 @@ import com.azure.cosmos.CosmosContainer;
 import com.azure.cosmos.CosmosDatabase;
 import com.azure.cosmos.models.CosmosItemResponse;
 import com.mongodb.*;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import jakarta.ws.rs.WebApplicationException;
@@ -46,9 +47,9 @@ public class MongoDBLayer {
     private MongoDatabase db;
 
     private MongoCollection<User> users;
-    private MongoCollection<Document> auctions;
-    private MongoCollection<Document> bids;
-    private MongoCollection<Document> questions;
+    private MongoCollection<Auction> auctions;
+    private MongoCollection<Bid> bids;
+    private MongoCollection<Question> questions;
 
 
     public MongoDBLayer(MongoClient client) { this.client = client;}
@@ -58,9 +59,9 @@ public class MongoDBLayer {
             return;
         db = client.getDatabase(DB_NAME);
         users = db.getCollection("users", User.class);
-        auctions = db.getCollection("auctions");
-        bids = db.getCollection("bids");
-        questions = db.getCollection("questions");
+        auctions = db.getCollection("auctions",  Auction.class);
+        bids = db.getCollection("bids",Bid.class);
+        questions = db.getCollection("questions", Question.class);
     }
 
     private void checkError(int error){
@@ -92,7 +93,7 @@ public class MongoDBLayer {
         init();
         User user = null;
         try{
-            Bson query = eq("_id",user.getId());
+            Bson query = eq("_id",userId);
             user = users.find(query).first();
         }catch (MongoException e) {
             checkError(e.getCode());
@@ -121,4 +122,134 @@ public class MongoDBLayer {
         }
         return user;
     }
+
+    public FindIterable<Auction> userAuctions(String userId){
+        init();
+        FindIterable<Auction> list = null;
+        try{
+            Bson query = eq("userId",userId);
+            list = auctions.find(query);
+        }catch (MongoException e) {
+            checkError(e.getCode());
+        }
+        return list;
+    }
+
+    //TODO ISTO TA ESTRANHO, teremos de filtrar este iterable?
+    public FindIterable<Bid> userFollowedAuctions(String userId){
+        init();
+        FindIterable<Bid> list = null;
+        try{
+            Bson query = eq("userId",userId);
+            list = bids.find(query);
+        }catch (MongoException e) {
+            checkError(e.getCode());
+        }
+        return list;
+    }
+
+    public Auction putAuction(Auction auction){
+        init();
+        try{
+            auctions.insertOne(auction);
+        }catch (MongoException e) {
+            checkError(e.getCode());
+        }
+        return auction;
+    }
+
+    public Auction getAuction(String auctionId){
+        init();
+        Auction auction = null;
+        try{
+            Bson query = eq("_id",auctionId);
+            auction = auctions.find(query).first();
+        }catch (MongoException e) {
+            checkError(e.getCode());
+        }
+        return auction;
+    }
+
+    public Auction updateAuction(String id, Auction auction){
+        init();
+        try{
+            Bson query = eq("_id",id);
+            auctions.replaceOne(query,auction);
+        }catch (MongoException e) {
+            checkError(e.getCode());
+        }
+        return auction;
+    }
+
+    public Bid putBid(Bid bid){
+        init();
+        try{
+            bids.insertOne(bid);
+        }catch (MongoException e) {
+            checkError(e.getCode());
+        }
+        return bid;
+    }
+
+    public FindIterable<Bid> listBids(String auctionId){
+        init();
+        FindIterable<Bid> list = null;
+        try{
+            Bson query = eq("auctionId",auctionId);
+            list = bids.find(query);
+        }catch (MongoException e) {
+            checkError(e.getCode());
+        }
+        return list;
+    }
+
+    public Question putQuestion(Question question){
+        init();
+        try{
+            questions.insertOne(question);
+        }catch (MongoException e) {
+            checkError(e.getCode());
+        }
+        return question;
+    }
+
+    public Question getQuestion(String questionId){
+        init();
+        Question question = null;
+        try{
+            Bson query = eq("_id",questionId);
+            question = questions.find(query).first();
+        }catch (MongoException e) {
+            checkError(e.getCode());
+        }
+        return question;
+    }
+
+    public Question replyToQuestion(String questionId,String reply){
+        init();
+        Question question = null;
+        try{
+            Bson query = eq("_id",questionId);
+            question = questions.find(query).first();
+            question.setReply(reply);
+            questions.replaceOne(query,question);
+        }catch (MongoException e) {
+            checkError(e.getCode());
+        }
+        return question;
+    }
+
+    public FindIterable<Question> listQuestions(String auctionId){
+        init();
+        FindIterable<Question> list = null;
+        try{
+            Bson query = eq("auctionId",auctionId);
+            list = questions.find(query);
+        }catch (MongoException e) {
+            checkError(e.getCode());
+        }
+        return list;
+    }
+
+    //todo missing closing auctions
 }
