ac75c4cd7b43384aafea5f64bee9cc582fd53c01
dmsRosa6 <dms.rosa6@gmail.com>
2025-06-06 21:35:23 +0100

    Fresh start + Spring

---
+++ kubeaction/src/main/java/com/dmsrosa/kubeaction/KubeactionApplication.java
@@ -0,0 +1,13 @@
+package com.dmsrosa.kubeaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubeactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubeactionApplication.class, args);
+	}
+
+}
